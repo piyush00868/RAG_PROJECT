9880bea8e4a1ef9293fd0ad051693c6f273c484a
piyush00868 <pk2857636@gmail.com>
2026-08-27 17:06:58 +0530

    Refactor document loading and summarization prompt in main.py

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 from dotenv import load_dotenv
-load_dotenv()
 
 from  langchain_mistralai import ChatMistralAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_community.document_loaders import TextLoader
+
+load_dotenv()
+
+data = TextLoader("document_loaders/notes.txt")
+docs = data.load()
+
+template = ChatPromptTemplate.from_messages([
+    ('system', "You are a helpful assistant that sumarizes the content of the document."),
+    ('human', "Summarize the following document: {data}")])
 
 model = ChatMistralAI(model="mistral-small-latest")
 
-response = model.invoke("What is python and use cases of it?")
+prompt = template.format_prompt(data=docs[0].page_content)
 
-print(response.text)
+response = model.invoke(prompt)
+
+print(response.content)
