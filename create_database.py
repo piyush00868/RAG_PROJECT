edd1f25222624f0452fb0bda2d23f94899a4a88a
piyush00868 <pk2857636@gmail.com>
2026-08-29 16:20:05 +0530

    fix: update PDF loader path to correct location

--- create_database.py
+++ create_database.py
@@ -11,7 +11,7 @@ from langchain_community.vectorstores import Chroma
 
 load_dotenv()
 
-loader = PyPDFLoader("recursive_split/designsystem.pdf")
+loader = PyPDFLoader("designsystem.pdf")
 docs = loader.load()
 
 splitter = RecursiveCharacterTextSplitter(
