40b1ac2ec015239274d2ee58757b99887b4dc3e2
piyush00868 <pk2857636@gmail.com>
2026-08-29 16:15:16 +0530

    feat: add text model for document summarization using ChatMistralAI

---
+++ document_loaders/text_model.py
@@ -0,0 +1,22 @@
+from dotenv import load_dotenv
+
+from  langchain_mistralai import ChatMistralAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_community.document_loaders import TextLoader
+
+load_dotenv()
+
+data = TextLoader("document_loaders/notes.txt")
+docs = data.load()
+
+template = ChatPromptTemplate.from_messages([
+    ('system', "You are a helpful assistant that sumarizes the content of the document."),
+    ('human', "Summarize the following document: {data}")])
+
+model = ChatMistralAI(model="mistral-small-latest")
+
+prompt = template.format_prompt(data=docs[0].page_content)
+
+response = model.invoke(prompt)
+
+print(response.content)
